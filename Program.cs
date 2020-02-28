583b1140573c865d762906d29f3b56896c6ebb2d
Krista Rutz <rutzkri000@gmail.com>
2020-02-28 12:06:46 -0800

    modularize code

--- Program.cs
+++ Program.cs
@@ -3,10 +3,11 @@ using BakeryService.Models;
 
 public class Program
 {
+  //public static int CustomerDollars = 1000;
   public static void Main()
   {
-    Bakery pierres = new Bakery();
+    Bakery pierres = new Bakery("Pierre's");
     pierres.WelcomeACustomer();
-
+    pierres.CustomersChoice();
   }
 }
