94411fb702aa2974b70792386de23de6a3c4d720
Krista Rutz <rutzkri000@gmail.com>
2020-02-28 10:25:59 -0800

    allow Program.cs to set the BakeryName, write a function to print the menu

--- Models/Bakery.cs
+++ Models/Bakery.cs
@@ -6,19 +6,42 @@ namespace BakeryService.Models
 {
   public class Bakery
   {
-    public Bakery()
-    {
+    public string BakeryName { get; set; }
+    public int BreadLoaves { get; set; } = 40;
+    public int Pastries { get; set; } = 100;
 
+    public Bakery(string bakeryName)
+    {
+      BakeryName = bakeryName;
     }
 
     public void WelcomeACustomer()
     {
-      Console.WriteLine("Welcome to Pierre's Bakery!");
-      Console.Write("How can we assist you today? ");
+      Console.WriteLine($"Welcome to {BakeryName} Bakery!");
+       Console.WriteLine();
+      Console.WriteLine("Here is today's menu:");
+      PrintDeals();
+      //PrintItemsInStock();
       if (ContinueOrExit())
       {
-        Console.WriteLine("Great! Here are today's specials:");
+        Console.Write("How can we assist you today? ");
       }
+    }
+
+    public void PrintDeals()
+    {
+      Console.WriteLine("");
+      Console.WriteLine("---------- M E N U ----------");
+      Console.WriteLine("Loaf of bread..............$5");
+      Console.WriteLine("Pastry.....................$2");
+      Console.WriteLine("-----------specials----------");
+      Console.WriteLine("Buy 2 loaves, get 1 free!....");
+      Console.WriteLine("Buy 3 pastries for $5!.......");
+    }
+
+    public void CustomersChoice()
+    {
+
     }
 
     public bool ContinueOrExit()
@@ -31,6 +54,7 @@ namespace BakeryService.Models
       }
       else if (choice == "2")
       {
+        System.Environment.Exit(0);
         return false;
       }
       else
